8352919471d16e0069a0b2b3972885ced299f18b
samiursakib <rafisamiur@gmail.com>
2024-10-24 01:46:41 +0600

    project initialized and global state context provider integrated

---
+++ src/app/providers/GlobalStateContextProvider.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useState,
+} from "react";
+
+type GlobalStateType = {
+  watchList: {
+    [key: string]: any;
+  }[];
+};
+
+type GlobalStateContextType = {
+  globalState: GlobalStateType;
+  setGlobalState: Dispatch<SetStateAction<GlobalStateType>>;
+};
+
+const GlobalStateContext = createContext({} as GlobalStateContextType);
+
+export const GlobalStateContextProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  const [globalState, setGlobalState] = useState<GlobalStateType>({
+    watchList: [],
+  });
+  return (
+    <GlobalStateContext.Provider value={{ globalState, setGlobalState }}>
+      {children}
+    </GlobalStateContext.Provider>
+  );
+};
+
+export const useGlobalState = () => {
+  return useContext(GlobalStateContext);
+};
